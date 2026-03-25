b6cd665672870d11d403289938407ce85d8cac01
RahulAIML <bhattacharyabuddhadeb147@gmail.com>
2026-03-26 02:20:34 +0530

    Set trust proxy for Render

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,9 @@ const { errorHandler } = require('./middleware/errorHandler');
 
 const app = express();
 
+// Render / reverse proxies send X-Forwarded-* headers
+app.set('trust proxy', 1);
+
 app.use(helmet());
 app.use(cors());
 app.use(express.json({ limit: '1mb' }));
